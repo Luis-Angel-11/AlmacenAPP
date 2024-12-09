ad7b1642a88d2acd7cfd7d649a2b88cbc38c3de9
Luis11 <leoluis5369@gmail.com>
2024-12-09 12:40:33 -0500

    se agrego los componente y botones al inventario

---
+++ src/clases/Productos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Productos {
+    private int id;
+    private String nombre;
+    private int stock;
+
+    public Productos(int id, String nombre, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+}
