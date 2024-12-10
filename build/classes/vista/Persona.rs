490307f3f41ff6b9b5f7fd546a0d4d511963ddd2
Ematixx <ematix0507@gmail.com>
2024-12-10 15:23:28 -0500

    cambios

---
+++ build/classes/vista/Persona.rs
@@ -0,0 +1,1 @@
+botonreporte.Persona
---
+++ build/classes/vista/frmformulario.rs
@@ -0,0 +1,6 @@
+botonreporte.frmformulario$5
+botonreporte.frmformulario$4
+botonreporte.frmformulario$1
+botonreporte.frmformulario$3
+botonreporte.frmformulario
+botonreporte.frmformulario$2
